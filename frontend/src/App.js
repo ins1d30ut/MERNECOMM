9941b5e08ff0ed73be8606064c63a7ac17affdad
ins1d30ut <nfteebsv@protonmail.com>
2022-08-09 01:00:17 -0400

    commit Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './componenets/Header';
+import Footer from './componenets/Footer';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
