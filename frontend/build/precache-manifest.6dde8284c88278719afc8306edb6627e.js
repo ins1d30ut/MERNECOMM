5dafb2b47e4bd7fd3e5b57bf3b8e3a5d8f4496c9
ins1d30ut <nfteebsv@protonmail.com>
2022-08-10 21:33:56 -0400

    commit all

---
+++ frontend/build/precache-manifest.6dde8284c88278719afc8306edb6627e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273a6ed396c38d688cb67d7a44757288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86afe44e7d8c81ad64d8",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86afe44e7d8c81ad64d8",
+    "url": "/static/js/main.b8932149.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
